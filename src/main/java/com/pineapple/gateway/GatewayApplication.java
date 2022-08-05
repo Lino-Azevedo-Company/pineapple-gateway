a1a30872e27a8dc07a89f65e2c2966cf6b7a0746
TobiasLino <linop019@gmail.com>
2022-08-05 14:33:17 -0300

    Add: security config

--- src/main/java/com/pineapple/gateway/GatewayApplication.java
+++ src/main/java/com/pineapple/gateway/GatewayApplication.java
@@ -1,13 +1,13 @@
 package com.pineapple.gateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableZuulProxy
 @SpringBootApplication
-@EnableAutoConfiguration
+@ComponentScan(basePackages = { "com.pineapple.commons", "com.pineapple.gateway"})
 public class GatewayApplication {
 
 	public static void main(String[] args) {
